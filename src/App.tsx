d4a9ebed8adee95d968cbf6fca80b3afd39f9f82
Pavlo Zalutskiy <kyrogane1@gmail.com>
2024-03-13 12:22:10 +0200

    feat: Add form markup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
-import './App.css'
+import './App.css';
 
 function App() {
-  return <div></div>;
+  return (
+    <form>
+      <label>
+        <span>ETH amount:</span>
+        <input type='number' />
+      </label>
+      <label>
+        <span>Action:</span>
+        <div>
+          <input type='checkbox' />
+          <span aria-live='assertive'>Sell</span>
+        </div>
+      </label>
+      <label>
+        <span>You will receive:</span>
+        <input type='text' disabled />
+      </label>
+    </form>
+  );
 }
 
-export default App
+export default App;
