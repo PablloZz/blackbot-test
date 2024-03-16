0d2f8a2f5ffe1422ae9ea2ca3ceeb9d869bced66
Pavlo Zalutskiy <kyrogane1@gmail.com>
2024-03-16 13:31:56 +0200

    feat: Move app component to components folder

---
+++ src/libs/components/app/app.tsx
@@ -0,0 +1,12 @@
+import styles from "./styles.module.css";
+import { EthUsdtForm } from "~/libs/components/components.ts";
+
+function App() {
+  return (
+    <main className={styles.main}>
+      <EthUsdtForm />
+    </main>
+  );
+}
+
+export { App };
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import { App } from "~/libs/components/components.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
